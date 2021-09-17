3537d1ed354b6348ab65533f5995377036bb3460
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2021-09-17 16:20:43 +0300

    chrome: introduce scss

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { App } from './pages';
 import reportWebVitals from './reportWebVitals';
 
-import './index.css';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
